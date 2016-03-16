193d84b2fdc1b41069d7bc2c96e7f86aff0c1347
Tomas Restrepo <tomas.restrepo@microsoft.com>
2016-03-16 14:12:59 -0500

    Clean up using directives

--- WCF/Shared/ClientIpTelemetryInitializer.cs
+++ WCF/Shared/ClientIpTelemetryInitializer.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 using Microsoft.ApplicationInsights.Channel;
 using Microsoft.ApplicationInsights.Extensibility.Implementation;
 using System.ServiceModel.Channels;
