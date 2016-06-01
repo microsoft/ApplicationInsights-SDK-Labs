fe1f5a0743fbd71ae34a8c0ae99535032da9d9fd
Tomas Restrepo <tomas.restrepo@microsoft.com>
2016-05-31 20:30:54 -0500

    Improve discovery of catch-all operations in contracts for the operation name

--- WCF/Shared/Implementation/WcfOperationContext.cs
+++ WCF/Shared/Implementation/WcfOperationContext.cs
@@ -134,6 +134,11 @@ namespace Microsoft.ApplicationInsights.Wcf.Implementation
             {
                 return catchAll.Name;
             }
+            var catchAll = operationContext.EndpointDispatcher.DispatchRuntime.UnhandledDispatchOperation;
+            if ( catchAll != null )
+            {
+                return catchAll.Name;
+            }
             return "*";
         }
 
