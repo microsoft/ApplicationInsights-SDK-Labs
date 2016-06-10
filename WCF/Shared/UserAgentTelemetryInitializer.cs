e8607e44b8b87dcb43b1e3756fe5ec1dffd62a98
Tomas Restrepo <tomas.restrepo@microsoft.com>
2016-06-09 20:45:11 -0500

    Avoid having to read HTTP Headers more than once on the UserAgent initializer

--- WCF/Shared/UserAgentTelemetryInitializer.cs
+++ WCF/Shared/UserAgentTelemetryInitializer.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.ApplicationInsights.Channel;
 using Microsoft.ApplicationInsights.Wcf.Implementation;
 using System;
+using Microsoft.ApplicationInsights.Extensibility.Implementation;
 
 namespace Microsoft.ApplicationInsights.Wcf
 {
@@ -16,13 +17,26 @@ namespace Microsoft.ApplicationInsights.Wcf
         /// <param name="operation">The operation context</param>
         protected override void OnInitialize(ITelemetry telemetry, IOperationContext operation)
         {
+            if ( String.IsNullOrEmpty(telemetry.Context.User.UserAgent) )
+            {
+                var userContext = telemetry.Context.User;
+                if ( String.IsNullOrEmpty(userContext.UserAgent) )
+                {
+                    UpdateUserAgent(operation, userContext);
+                }
+                telemetry.Context.User.UserAgent = userContext.UserAgent;
+            }
+        }
+
+        private void UpdateUserAgent(IOperationContext operation, UserContext userContext)
+        {
             var httpHeaders = operation.GetHttpRequestHeaders();
             if ( httpHeaders != null )
             {
                 var userAgent = httpHeaders.Headers["User-Agent"];
                 if ( !String.IsNullOrEmpty(userAgent) )
                 {
-                    telemetry.Context.User.UserAgent = userAgent;
+                    userContext.UserAgent = userAgent;
                 }
             }
         }
