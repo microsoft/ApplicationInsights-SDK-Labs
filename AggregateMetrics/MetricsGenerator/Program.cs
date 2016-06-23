d4727257f478de0e670a30220e6b1ce222590c9f
Sergey Kanzhelev <sergkanz@microsoft.com>
2016-06-23 14:34:11 -0700

    added a simple test for the API

--- AggregateMetrics/MetricsGenerator/Program.cs
+++ AggregateMetrics/MetricsGenerator/Program.cs
@@ -4,22 +4,67 @@
     using System.Threading.Tasks;
     using Microsoft.ApplicationInsights;
     using Microsoft.ApplicationInsights.Extensibility.AggregateMetrics.One;
-
+    using Microsoft.ApplicationInsights.Extensibility.AggregateMetrics.Two;
+    using System.Diagnostics;
+    using System.Collections.Generic;
+    using System.Collections.Concurrent;
+    using System.Threading;
     class Program
     {
         static void Main(string[] args)
         {
             var telemetryClient = new TelemetryClient();
 
-            telemetryClient.RegisterAggregateMetric("MetricsGenerator", "City", percentileCalculation: PercentileCalculation.OrderByLargest);
+            
+            //API one:
 
-            for (double i = -50; i < 10000000; i += new Random().NextDouble())
+            //telemetryClient.RegisterAggregateMetric("MetricsGenerator", "City", percentileCalculation: PercentileCalculation.OrderByLargest);
+
+            //for (double i = -50; i < 10000000; i += new Random().NextDouble())
+            //{
+            //    telemetryClient.TrackAggregateMetric("MetricsGenerator", i, "Seattle");
+            //    telemetryClient.TrackAggregateMetric("MetricsGenerator", i, "New York");
+            //}
+
+
+            //API two:
+
+            Random rand = new Random();
+            telemetryClient.Gauge("active processes", () => { return rand.Next(10, 15); });
+
+            var counter = telemetryClient.Counter("# of items");
+
+            var meter = telemetryClient.Meter("rate of items");
+            var aMeter = telemetryClient.Meter("rate of char a");
+
+            while (true)
             {
-                telemetryClient.TrackAggregateMetric("MetricsGenerator", i, "Seattle");
-                telemetryClient.TrackAggregateMetric("MetricsGenerator", i, "New York");
-            }
+                var queue = new ConcurrentQueue<char>();
+                var a = Console.ReadKey().KeyChar;
+                queue.Enqueue(a);
+                counter.Increment();
+                meter.Mark();
+                if (a == 'a')
+                {
+                    aMeter.Mark();
+                }
+                else
+                {
+                    aMeter.Mark(0);
+                }
 
-            Task.Delay(20 * 1000).Wait();
+                new Task(() =>
+                {
+                    Thread.Sleep(100);
+                    char ch;
+                    if (queue.TryDequeue(out ch))
+                    {
+                        counter.Decrement();
+                        Console.Write(ch);
+                    }
+                }).Start();
+            }
+            
         }
     }
 }
