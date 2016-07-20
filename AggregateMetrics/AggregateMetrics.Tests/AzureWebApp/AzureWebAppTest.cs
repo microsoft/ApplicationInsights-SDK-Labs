bbd8240ea5e0041245053e3e152f1eed076b4f53
t-mafra <t-mafra@MACKCOMP>
2016-07-20 10:46:43 -0700

    first draft using cached class

---
+++ AggregateMetrics/AggregateMetrics.Tests/AzureWebApp/AzureWebAppTest.cs
@@ -0,0 +1,25 @@
+﻿namespace webAppTest
+{
+    using System;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Microsoft.ApplicationInsights.Extensibility.AggregateMetrics.AzureWebApp;
+    using Microsoft.ApplicationInsights.DataContracts;
+    using System.Diagnostics;
+    [TestClass]
+    public class UnitTestAzureWeb
+    {
+        [TestMethod]
+        public void TestGetValueAndReset()
+        {
+            PerformanceCounterGauge testingGage = new PerformanceCounterGauge();
+
+            var mt= testingGage.GetValueAndReset();
+        
+            Debug.WriteLine("mt contents:");
+            Debug.WriteLine(mt.Value);
+            Debug.WriteLine(mt.Name);
+
+            Assert.IsTrue(testingGage.GetValueAndReset() is MetricTelemetry );
+        }
+    }
+}
