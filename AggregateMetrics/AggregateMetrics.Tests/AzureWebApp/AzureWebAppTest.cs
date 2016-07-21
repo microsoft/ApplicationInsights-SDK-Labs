8bb69dbe0f5e03fc9e865ab7b1487f89a221028b
t-mafra <t-mafra@MACKCOMP>
2016-07-20 17:32:07 -0700

    StyleCop compliance and more multi-counter functionality

--- AggregateMetrics/AggregateMetrics.Tests/AzureWebApp/AzureWebAppTest.cs
+++ AggregateMetrics/AggregateMetrics.Tests/AzureWebApp/AzureWebAppTest.cs
@@ -2,24 +2,24 @@
 {
     using System;
     using Microsoft.VisualStudio.TestTools.UnitTesting;
-    using Microsoft.ApplicationInsights.Extensibility.AggregateMetrics.AzureWebApp;
     using Microsoft.ApplicationInsights.DataContracts;
     using System.Diagnostics;
+    using Microsoft.ApplicationInsights.Extensibility.AggregateMetrics.AzureWebApp;
     [TestClass]
     public class UnitTestAzureWeb
     {
         [TestMethod]
         public void TestGetValueAndReset()
         {
-            PerformanceCounterGauge testingGage = new PerformanceCounterGauge();
+            FlexiblePerformanceCounterGauge testingGage = new FlexiblePerformanceCounterGauge();
 
-            var mt= testingGage.GetValueAndReset();
+            var metric= testingGage.GetRawCounterValue("privateBytes");
         
             Debug.WriteLine("mt contents:");
-            Debug.WriteLine(mt.Value);
-            Debug.WriteLine(mt.Name);
+            Debug.WriteLine(metric.Value);
+            Debug.WriteLine(metric.Name);
 
-            Assert.IsTrue(testingGage.GetValueAndReset() is MetricTelemetry );
+            Assert.IsTrue(testingGage.GetRawCounterValue("privateBytes") is MetricTelemetry );
         }
     }
 }
