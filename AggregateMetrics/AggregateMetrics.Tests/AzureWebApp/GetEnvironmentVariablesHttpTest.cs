d87901a0a68f5fa63ff89341c0878937da65ba65
Mateo Torres Ruiz <mateo.atr@gmail.com>
2016-07-21 15:35:19 -0700

    Implement Mackenzie's CacheHelper as a singleton. Get environment variables from azure.

---
+++ AggregateMetrics/AggregateMetrics.Tests/AzureWebApp/GetEnvironmentVariablesHttpTest.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.ApplicationInsights.Extensibility.AggregateMetrics.AzureWebApp;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AggregateMetrics.Tests.AzureWebApp
+{
+    [TestClass]
+    public class GetEnvironmentVariablesHttpTest : IPerformanceCounter
+    {
+        [TestMethod]
+        public string GetAzureWebAppEnvironmentVariables(AzureWebApEnvironmentVariables environmentVaribale)
+        {
+            HttpClient client = new HttpClient();
+
+            Task<string> task = client.GetStringAsync("http://remoteenvironmentvariables.azurewebsites.net/api/EnvironmentVariables/WEBSITE_COUNTERS_ASPNET/");
+
+            task.Wait();
+
+            string json = task.Result;
+
+            Assert.IsTrue(json.Contains("applicationRestarts"));
+
+            return json;
+        }
+    }
+}
