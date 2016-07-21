984de67f1630d1729aca265a24f738e7a2b509ef
Mateo Torres Ruiz <mateo.atr@gmail.com>
2016-07-21 16:33:46 -0700

    Add performance collector

--- AggregateMetrics/AggregateMetrics/AzureWebApp/PerformanceCollectorModule.cs
+++ AggregateMetrics/AggregateMetrics/AzureWebApp/PerformanceCollectorModule.cs
@@ -1,12 +1,37 @@
-﻿using System;
+﻿using Microsoft.ApplicationInsights.Extensibility.AggregateMetrics.Two;
+using Microsoft.ApplicationInsights.Extensibility.PerfCounterCollector;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Microsoft.ApplicationInsights.Extensibility.AggregateMetrics.AzureWebApp
 {
-    class PerformanceCollectorModule
+    public class PerformanceCollectorModule : ITelemetryModule
     {
+        private readonly List<string> defaultCounters = new List<string>()
+                                                            {
+                                                                @"\Process(??APP_WIN32_PROC??)\% Processor Time",
+                                                                @"\Memory\Available Bytes",
+                                                                @"\ASP.NET Applications(??APP_W3SVC_PROC??)\Requests/Sec",
+                                                                @"\.NET CLR Exceptions(??APP_CLR_PROC??)\# of Exceps Thrown / sec",
+                                                                @"\ASP.NET Applications(??APP_W3SVC_PROC??)\Request Execution Time",
+                                                                @"\Process(??APP_WIN32_PROC??)\Private Bytes",
+                                                                @"\Process(??APP_WIN32_PROC??)\IO Data Bytes/sec",
+                                                                @"\ASP.NET Applications(??APP_W3SVC_PROC??)\Requests In Application Queue",
+                                                                @"\Processor(_Total)\% Processor Time"
+                                                            };
+
+        public IList<PerformanceCounterCollectionRequest> Counters { get; private set; }
+
+        public void Initialize(TelemetryConfiguration configuration)
+        {
+            FlexiblePerformanceCounterGauge gauge = new FlexiblePerformanceCounterGauge("privateBytes");
+            configuration.RegisterCounter(gauge);
+
+        }
     }
 }
