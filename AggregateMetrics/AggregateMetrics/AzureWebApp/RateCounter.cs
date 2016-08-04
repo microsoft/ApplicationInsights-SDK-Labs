3e11a8d93cd04caad264fc6389e38fc988ab589f
Mateo Torres Ruiz <mateo.atr@gmail.com>
2016-08-03 22:36:48 -0700

    RateCounter implements ICounterValue.

--- AggregateMetrics/AggregateMetrics/AzureWebApp/RateCounter.cs
+++ AggregateMetrics/AggregateMetrics/AzureWebApp/RateCounter.cs
@@ -1,21 +1,55 @@
-﻿using Microsoft.ApplicationInsights.DataContracts;
-using System;
-
-namespace Microsoft.ApplicationInsights.Extensibility.AggregateMetrics.AzureWebApp
+﻿namespace Microsoft.ApplicationInsights.Extensibility.AggregateMetrics.AzureWebApp
 {
+    using System;
+    using Microsoft.ApplicationInsights.DataContracts;
+    using Microsoft.ApplicationInsights.Extensibility.AggregateMetrics.Two;
+
     /// <summary>
     /// Struct for metrics dependant on time.
     /// </summary>
-    public struct RateCounter
+    public class RateCounter : ICounterValue
     {
-        /// <summary>
-        /// Metric object.
-        /// </summary>
-        public MetricTelemetry MetricTelemetry;
+        private string name;
+
+        private double? lastValue;
 
         /// <summary>
         /// DateTime object to keep track of the last time this metric was retrieved.
         /// </summary>
-        public DateTime DateTime;
+        private DateTime dateTime;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="RateCounter"/> class.
+        /// </summary>
+        /// <param name="name"> Name of counter variable.</param>
+        public RateCounter(string name)
+        {
+            this.name = name;
+        }
+
+        /// <summary>
+        /// Returns the current value of the rate counter if enough information exists.
+        /// </summary>
+        /// <returns> MetricTelemetry object.</returns>
+        public MetricTelemetry GetValueAndReset()
+        {
+            MetricTelemetry metric = new MetricTelemetry();
+
+            metric.Name = this.name;
+
+            if (this.lastValue == null)
+            {
+                this.lastValue = CacheHelper.Instance.GetCounterValue(this.name);
+                this.dateTime = System.DateTime.Now;
+
+                return metric;
+            }
+
+            metric.Value = ((double)this.lastValue - CacheHelper.Instance.GetCounterValue(this.name)) / (System.DateTime.Now.Second - this.dateTime.Second);
+            this.lastValue = CacheHelper.Instance.GetCounterValue(this.name);
+            this.dateTime = System.DateTime.Now;
+
+            return metric;
+        }
     }
 }
