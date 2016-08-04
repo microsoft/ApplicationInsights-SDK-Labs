9b2a7909aa6984a06c62da3db3dd307bed3de322
Mateo Torres Ruiz <mateo.atr@gmail.com>
2016-08-03 21:53:53 -0700

    Add SumUpGauge class, StyleCop compliant modifications in FlexiblePerformanceCountersGauge.

--- AggregateMetrics/AggregateMetrics/AzureWebApp/DefaultCounters.cs
+++ AggregateMetrics/AggregateMetrics/AzureWebApp/DefaultCounters.cs
@@ -1,51 +1,62 @@
-﻿using Microsoft.ApplicationInsights.DataContracts;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-
-namespace Microsoft.ApplicationInsights.Extensibility.AggregateMetrics.AzureWebApp
+﻿namespace Microsoft.ApplicationInsights.Extensibility.AggregateMetrics.AzureWebApp
 {
+    using System.Collections.Generic;
+    using System.Linq;
+    using Microsoft.ApplicationInsights.DataContracts;
+
+    /// <summary>
+    /// Class that gives the user the default performance counters.
+    /// </summary>
     internal class DefaultCounters
     {
-        private static readonly DefaultCounters instance = new DefaultCounters();
+        private static readonly DefaultCounters DefaultCountersInstance = new DefaultCounters();
 
-        private DefaultCounters() { this.Initialize(); }
+        Dictionary<FlexiblePerformanceCounterGauge, MetricTelemetry> defaultCounters;
+        Dictionary<FlexiblePerformanceCounterGauge, RateCounter> rateDefaultCounters;
+
+        SumUpGauge processorTime = new SumUpGauge(
+                "processorTime",
+                new FlexiblePerformanceCounterGauge("kernelTime"),
+                new FlexiblePerformanceCounterGauge("userTime"));
+
+        SumUpGauge ioDataBytesRate = new SumUpGauge(
+                "ioDataBytesRate",
+                new FlexiblePerformanceCounterGauge("readIoBytes"),
+                new FlexiblePerformanceCounterGauge("writeIoBytes"),
+                new FlexiblePerformanceCounterGauge("otherIoBytes"));
+
+        private DefaultCounters()
+        {
+            this.Initialize();
+        }
 
         /// <summary>
-        /// Return the only instance of DefaultCounters.
+        /// Gets the only instance of DefaultCounters.
         /// </summary>
         public static DefaultCounters Instance
         {
             get
             {
-                return instance;
+                return DefaultCountersInstance;
             }
         }
-
-        Dictionary<FlexiblePerformanceCounterGauge, MetricTelemetry> defaultCounters;
-        Dictionary<FlexiblePerformanceCounterGauge, RateCounter> timeDependentCounters;
 
         /// <summary>
         /// Initializes the dictionaries for the default performance counters.
         /// </summary>
         public void Initialize()
         {
-            defaultCounters = new Dictionary<FlexiblePerformanceCounterGauge, MetricTelemetry>()
+            this.defaultCounters = new Dictionary<FlexiblePerformanceCounterGauge, MetricTelemetry>()
             {
-                { new FlexiblePerformanceCounterGauge("kernelTime"), new MetricTelemetry() },
-                { new FlexiblePerformanceCounterGauge("userTime"), new MetricTelemetry() },
                 { new FlexiblePerformanceCounterGauge("appRequestExecTime"), new MetricTelemetry() },
                 { new FlexiblePerformanceCounterGauge("privateBytes"), new MetricTelemetry() },
                 { new FlexiblePerformanceCounterGauge("requestsInApplicationQueue"), new MetricTelemetry() }
             };
 
-            timeDependentCounters = new Dictionary<FlexiblePerformanceCounterGauge, RateCounter>()
+            this.rateDefaultCounters = new Dictionary<FlexiblePerformanceCounterGauge, RateCounter>()
             {
                 { new FlexiblePerformanceCounterGauge("requestsTotal"), new RateCounter() },
-                { new FlexiblePerformanceCounterGauge("exceptionsThrown"), new RateCounter() },
-                { new FlexiblePerformanceCounterGauge("readIoBytes"), new RateCounter() },
-                { new FlexiblePerformanceCounterGauge("writeIoBytes"), new RateCounter() },
-                { new FlexiblePerformanceCounterGauge("otherIoBytes"), new RateCounter() }
+                { new FlexiblePerformanceCounterGauge("exceptionsThrown"), new RateCounter() }
             };
         }
 
@@ -53,26 +64,19 @@ namespace Microsoft.ApplicationInsights.Extensibility.AggregateMetrics.AzureWebA
         /// Gets the default performance counters.
         /// </summary>
         /// <returns> List of MetricTelemetry objects</returns>
-        private List<MetricTelemetry> GetCounters(bool useEnvironmentVariables = true)
+        private List<MetricTelemetry> GetCounters()
         {
             List<MetricTelemetry> metrics = new List<MetricTelemetry>();
 
-            foreach (var item in defaultCounters.ToList())
+            foreach (var item in this.defaultCounters.ToList())
             {
-                if (useEnvironmentVariables)
-                    defaultCounters[item.Key] = item.Key.GetValueAndReset();
-                else
-                {
-                    MetricTelemetry metric = new MetricTelemetry();
-                    metric.Value = defaultCounters[item.Key].Value;
-                }
-
-                metrics.Add(defaultCounters[item.Key]);
+                this.defaultCounters[item.Key] = item.Key.GetValueAndReset();
+                metrics.Add(this.defaultCounters[item.Key]);
             }
 
-            foreach (var item in timeDependentCounters.ToList())
+            foreach (var item in this.rateDefaultCounters.ToList())
             {
-                bool firstValue = timeDependentCounters[item.Key].DateTime == System.DateTime.MinValue;
+                bool firstValue = this.rateDefaultCounters[item.Key].DateTime == System.DateTime.MinValue;
 
                 RateCounter counter = new RateCounter();
                 counter.MetricTelemetry = item.Key.GetValueAndReset();
@@ -81,11 +85,11 @@ namespace Microsoft.ApplicationInsights.Extensibility.AggregateMetrics.AzureWebA
                 if (!firstValue)
                 {
                     MetricTelemetry metric = counter.MetricTelemetry;
-                    metric.Value = (counter.MetricTelemetry.Value - timeDependentCounters[item.Key].MetricTelemetry.Value) / (counter.DateTime.Second - timeDependentCounters[item.Key].DateTime.Second);
+                    metric.Value = (counter.MetricTelemetry.Value - this.rateDefaultCounters[item.Key].MetricTelemetry.Value) / (counter.DateTime.Second - this.rateDefaultCounters[item.Key].DateTime.Second);
                     metrics.Add(metric);
                 }
 
-                timeDependentCounters[item.Key] = counter;
+                this.rateDefaultCounters[item.Key] = counter;
             }
 
             return metrics;
---
+++ AggregateMetrics/AggregateMetrics/AzureWebApp/SumUpGauge.cs
@@ -0,0 +1,51 @@
+﻿namespace Microsoft.ApplicationInsights.Extensibility.AggregateMetrics.AzureWebApp
+{
+    using System.Collections.Generic;
+    using Microsoft.ApplicationInsights.DataContracts;
+    using Microsoft.ApplicationInsights.Extensibility.AggregateMetrics.Two;
+
+    /// <summary>
+    /// Gauge that sums up the values of different gauges.
+    /// </summary>
+    public class SumUpGauge : ICounterValue
+    {
+        private string name;
+
+        private List<FlexiblePerformanceCounterGauge> gaugesToSum;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SumUpGauge"/> class.
+        /// </summary>
+        /// <param name="name"> Name of the SumUpGauge.</param>
+        /// <param name="gauges"> Gauges to sum.</param>
+        public SumUpGauge(string name, params FlexiblePerformanceCounterGauge[] gauges)
+        {
+            this.name = name;
+            this.gaugesToSum = new List<FlexiblePerformanceCounterGauge>();
+
+            foreach (FlexiblePerformanceCounterGauge gauge in gauges)
+            {
+                this.gaugesToSum.Add(gauge);
+            }
+        }
+
+        /// <summary>
+        /// Returns the current value of the sum of all different gauges attached to this one and resets their values.
+        /// </summary>
+        /// <returns> MetricTelemetry object</returns>
+        public MetricTelemetry GetValueAndReset()
+        {
+            var metric = new MetricTelemetry();
+
+            metric.Name = this.name;
+            metric.Value = 0;
+
+            foreach (FlexiblePerformanceCounterGauge gauge in this.gaugesToSum)
+            {
+                metric.Value += gauge.GetValueAndReset().Value;
+            }
+
+            return metric;
+        }
+    }
+}
