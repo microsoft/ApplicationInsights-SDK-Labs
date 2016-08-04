b19432dff9540a6538ddb06714e3fc1a608b92c3
Mateo Torres Ruiz <mateo.atr@gmail.com>
2016-08-04 13:45:38 -0700

    Remove dynamic type. Implement ICacheHelper interface.

---
+++ AggregateMetrics/AggregateMetrics.Tests/AzureWebApp/RateCounterTests.cs
@@ -0,0 +1,25 @@
+﻿namespace AggregateMetrics.Tests.AzureWebApp
+{
+    using Microsoft.ApplicationInsights.DataContracts;
+    using Microsoft.ApplicationInsights.Extensibility.AggregateMetrics.AzureWebApp;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class RateCounterTests
+    {
+        [TestMethod]
+        public void RateCounterGaugeGetValueAndResetWorking()
+        {
+            RateCounterGauge privateBytesRate = new RateCounterGauge("privateBytes", new CacheHelperTests());
+
+            MetricTelemetry metric = privateBytesRate.GetValueAndReset();
+            Assert.IsTrue(metric.Value == 0);
+
+            System.Threading.Thread.Sleep(System.TimeSpan.FromSeconds(7));
+
+            metric = privateBytesRate.GetValueAndReset();
+
+            Assert.IsTrue(metric.Value != 0);
+        }
+    }
+}
