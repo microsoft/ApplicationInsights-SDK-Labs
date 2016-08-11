3019f9fbbed543d22b511a78a68a36fb9f8a5b21
Mateo Torres Ruiz <mateo.atr@gmail.com>
2016-08-11 14:41:49 -0700

    Check if app. is running in Azure before trying to register gauges. Use ReportAs value if set by user.

--- AggregateMetrics/AggregateMetrics/AzureWebApp/Implementation/CounterFactory.cs
+++ AggregateMetrics/AggregateMetrics/AzureWebApp/Implementation/CounterFactory.cs
@@ -9,42 +9,57 @@
     internal class CounterFactory
     {
         /// <summary>
+        /// Set metrics alias to be the value given by the user.
+        /// </summary>
+        /// <param name="counterName">Name of the counter to retrieve.</param>
+        /// <param name="reportAs">Alias to report the counter.</param>
+        /// <returns>Alias that will be used for the counter.</returns>
+        private string SetCounterName(string counterName, string reportAs)
+        {
+            if (reportAs == null)
+                return counterName;
+            else
+                return reportAs;
+        }
+
+        /// <summary>
         /// Gets a counter.
         /// </summary>
         /// <param name="counterName">Name of the counter to retrieve.</param>
+        /// <param name="reportAs">Alias to report the counter under.</param>
         /// <returns>The counter identified by counterName</returns>
-        public ICounterValue GetCounter(string counterName)
+        public ICounterValue GetCounter(string counterName, string reportAs = null)
         {
             switch (counterName)
             {
                 case @"\ASP.NET Applications(??APP_W3SVC_PROC??)\Request Execution Time":
                     return new PerformanceCounterFromJsonGauge(
-                        counterName,
+                        SetCounterName(counterName, reportAs),
                         "appRequestExecTime");
                 case @"\Process(??APP_WIN32_PROC??)\Private Bytes":
                     return new PerformanceCounterFromJsonGauge(
-                        counterName,
+                        SetCounterName(counterName, reportAs),
                         "privateBytes");
                 case @"\ASP.NET Applications(??APP_W3SVC_PROC??)\Requests In Application Queue":
                     return new PerformanceCounterFromJsonGauge(
-                        counterName,
+                        SetCounterName(counterName, reportAs),
                         "requestsInApplicationQueue");
                 case @"\ASP.NET Applications(??APP_W3SVC_PROC??)\Requests/Sec":
                     return new RateCounterGauge(
-                        counterName, 
+                        SetCounterName(counterName, reportAs), 
                         "requestsTotal");
                 case @"\.NET CLR Exceptions(??APP_CLR_PROC??)\# of Exceps Thrown / sec":
                     return new RateCounterGauge(
-                        counterName,
+                        SetCounterName(counterName, reportAs),
                         "exceptionsThrown");
                 case @"\Processor(_Total)\% Processor Time":
                     return new SumUpGauge(
-                        counterName,
+                        SetCounterName(counterName, reportAs),
                         new PerformanceCounterFromJsonGauge("kernelTime", "kernelTime"),
                         new PerformanceCounterFromJsonGauge("userTime", "userTime"));
                 case @"\Process(??APP_WIN32_PROC??)\IO Data Bytes/sec":
                     return new RateCounterGauge(
-                        counterName, 
+                        SetCounterName(counterName, reportAs), 
                         "ioDataBytesRate",
                         new SumUpGauge(
                             "ioDataBytesRate",
@@ -59,11 +74,11 @@
                                 "otherIoBytes")));
                 case @"\Process(??APP_WIN32_PROC??)\Handle Count":
                     return new PerformanceCounterFromJsonGauge(
-                        counterName,
+                        SetCounterName(counterName, reportAs),
                         "handles");
                 case @"\Process(??APP_WIN32_PROC??)\Thread Count":
                     return new PerformanceCounterFromJsonGauge(
-                        counterName,
+                        SetCounterName(counterName, reportAs),
                         "threads");
                 default:
                     throw new ArgumentException("Performance counter was not found.", counterName);
