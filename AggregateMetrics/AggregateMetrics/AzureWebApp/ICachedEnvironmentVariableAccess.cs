5a0f5b486d3212f50a61ea9b6b28c48aadfea3fd
Mateo Torres Ruiz <mateo.atr@gmail.com>
2016-08-16 10:15:01 -0700

    Code compliant with StyleCop

--- AggregateMetrics/AggregateMetrics/AzureWebApp/ICachedEnvironmentVariableAccess.cs
+++ AggregateMetrics/AggregateMetrics/AzureWebApp/ICachedEnvironmentVariableAccess.cs
@@ -9,6 +9,7 @@
         /// Returns value of a counter from cache.
         /// </summary>
         /// <param name="name"> Name of the counter.</param>
+        /// <param name="environmentVariable"> Identifier of the corresponding environment variable.</param>
         /// <returns> Counter value.</returns>
         int GetCounterValue(string name, AzureWebApEnvironmentVariables environmentVariable);
     }
