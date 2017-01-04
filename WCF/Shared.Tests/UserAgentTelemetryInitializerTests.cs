dc97499bfab7add625d67fd84b2ef63af3095afe
Tomas Restrepo <tomas.restrepo@microsoft.com>
2017-01-03 20:02:48 -0500

    Copy user agent information from the request telemetry if available

--- WCF/Shared.Tests/UserAgentTelemetryInitializerTests.cs
+++ WCF/Shared.Tests/UserAgentTelemetryInitializerTests.cs
@@ -25,5 +25,19 @@ namespace Microsoft.ApplicationInsights.Wcf.Tests
 
             Assert.AreEqual("MyUserAgent", telemetry.Context.User.UserAgent);
         }
+        [TestMethod]
+        public void UserAgentIsCopiedFromRequestIfPresent()
+        {
+            var context = new MockOperationContext();
+            context.EndpointUri = new Uri("http://localhost/Service1.svc");
+            context.OperationName = "GetData";
+            context.Request.Context.User.UserAgent = "MyUserAgent";
+
+            var initializer = new UserAgentTelemetryInitializer();
+            var telemetry = new EventTelemetry();
+            initializer.Initialize(telemetry, context);
+
+            Assert.AreEqual(context.Request.Context.User.UserAgent, telemetry.Context.User.UserAgent);
+        }
     }
 }
